83db81ce863f09b8b597e55a0833ece395e01627
Daniel Serrano <ingm.daniel.serrano@gmail.com>
2021-07-19 01:38:40 -0500

    Home styled

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 // Interfaces
 import { Comic } from 'src/app/interfaces/comic';
 import { Serie } from 'src/app/interfaces/serie';
-// services
-import { ComicService } from '../../services/comic.service';
-import { SeriesService } from '../../services/series.service';
+// servicese';
 // env variables
 import { environment } from '../../../environments/environment';
 @Component({
@@ -13,52 +11,14 @@ import { environment } from '../../../environments/environment';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  comics: Comic[] = [];
-  comicsImg: string = '';
+  comicsImg: string = 'https://i.annihil.us/u/prod/marvel/i/mg/6/a0/60e5e1550f77e.jpg';
   comicsUrl: string = `${environment.baseUrl}/comics`;
-  series: Serie[] = [];
-  seriesImg: string = '';
+  seriesImg: string = 'https://i.annihil.us/u/prod/marvel/i/mg/1/40/5e558a8495066.jpg';
   seriessUrl: string = `${environment.baseUrl}/series`;
-  loading: boolean = false;
-  isError: boolean = false;
+
   
-  constructor(private comicService: ComicService,
-    private seriesService: SeriesService) {}
+  constructor() {}
   ngOnInit(): void {
-    this.getComics();
-    this.getSeries();
-    console.log(' : ', window.location.pathname )
-    
-  }
-  getComics(): void {
-    this.comicService.getComics().subscribe((result: any) => {
-      this.comics = result.data.results;
-      let index = this.comics.reduce((acc, element, index) =>
-        !element.thumbnail.path.endsWith('image_not_available') ? index : acc
-      , 0);
-      this.comicsImg = `${this.comics[index].thumbnail.path}.${this.comics[index].thumbnail.extension}`;
-      this.loading = false;
-      console.log( 'Data : ', this.comics, this.comicsImg )
-    }, (serrviceError => {
-      this.isError = true;
-      this.loading = false;
-      console.log('serviceError : ', serrviceError);
-    } ) );
   }
 
-  getSeries(): void {
-    this.seriesService.getSeries().subscribe((result: any) => {
-      this.series = result.data.results;
-      let index = this.series.reduce((acc, element, index) =>
-        !element.thumbnail.path.endsWith('image_not_available') ? index : acc
-      , 0);
-      this.seriesImg = `${this.series[index].thumbnail.path}.${this.series[index].thumbnail.extension}`;
-      this.loading = false;
-      console.log( 'Data : ', this.series )
-    }, (serrviceError => {
-      this.isError = true;
-      this.loading = false;
-      console.log('serviceError : ', serrviceError);
-    } ) );
-  }
 }
